0d359a6c854cc897bf31d771534de4864b73c746
seeubot <155578467+seeubot@users.noreply.github.com>
2026-08-04 10:30:08 +0530

    Create version.js

---
+++ api/version.js
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    version: "1.0.1",
+    versionCode: 2,
+    updateUrl: "https://your-download-link.com/app-release.apk",
+    forceUpdate: false,
+    message: "New channels added! Zee Tamil, Zee Bangala, and more.",
+    releaseDate: "2026-08-04"
+  });
+}

e28405a28f192d9fdc25ec982ca2aa4575956da6
seeubot <155578467+seeubot@users.noreply.github.com>
2026-08-04 10:44:46 +0530

    Update version.js

--- api/version.js
+++ api/version.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   res.status(200).json({
     version: "1.0.1",
-    versionCode: 2,
+    versionCode: 1,
     updateUrl: "https://your-download-link.com/app-release.apk",
     forceUpdate: true,
     message: "New channels added! Zee Tamil, Zee Bangala, and more.",

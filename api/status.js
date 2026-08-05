96b930b634b920bf12b08b146854cd0fc9cc8a22
seeubot <155578467+seeubot@users.noreply.github.com>
2026-08-05 10:38:43 +0530

    Update status.js

--- api/status.js
+++ api/status.js
@@ -6,7 +6,7 @@ export default function handler(req, res) {
   // ===== CONFIGURATION - Edit these to control the app =====
   const config = {
     // Set to true to show maintenance screen
-    maintenance: false,
+    maintenance: true,
     
     // Set to true to force update
     update: false,

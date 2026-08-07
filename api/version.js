57cedf26a17da4cbb28a63f32c87766c97e23173
seeubot <155578467+seeubot@users.noreply.github.com>
2026-08-07 19:22:44 +0530

    Create version.js

---
+++ api/version.js
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    version: "1.0.0",
+    versionCode: 1,
+    updateUrl: "https://t.me/+PEc2GpjasuExYzFl",
+    forceUpdate: false,
+    message: "Welcome to Chill Box IPTV!",
+    releaseDate: "2026-08-07"
+  });
+}

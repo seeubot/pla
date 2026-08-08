ca3d34356b62a3f26df9c24e8d617ea87f1c343d
seeubot <155578467+seeubot@users.noreply.github.com>
2026-08-08 23:10:54 +0530

    Update playlist.js

--- api/playlist.js
+++ api/playlist.js
@@ -98,7 +98,7 @@ export default async function handler(req, res) {
               clearKey: srv.drm ? srv.license : null,
               url: srv.url
             };
-            // if (!shouldKeep(flat, filter)) continue;
+            if (!shouldKeep(flat, filter)) continue;
             addCh(channelMap, flat);
           }
         }
@@ -152,7 +152,7 @@ function fetchUrl(url) {
         'Accept': '*/*',
         'Accept-Language': 'en-US,en;q=0.9',
       },
-      timeout: 8000
+      timeout: 20000
     }, (response) => {
       if (response.statusCode >= 300 && response.statusCode < 400 && response.headers.location) {
         fetchUrl(response.headers.location).then(resolve);

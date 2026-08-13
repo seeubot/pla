5cacb9e5be5885f03fdd2347abc1bd2f2cee2c93
seeubot <155578467+seeubot@users.noreply.github.com>
2026-08-13 11:47:28 +0530

    Create status.js

---
+++ api/status.js
@@ -0,0 +1,20 @@
+export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(204).end();
+  }
+
+  res.status(200).json({
+    maintenance: false,
+    update: true, // Set to true when you want to force update
+    version: "1.0.1",
+    versionCode: 2,
+    title: "Update Required",
+    message: "Please update Chill Box to the latest version",
+    apkUrl: "https://serverfile-sigma.vercel.app/chillbox-v1.0.1.apk",
+    telegram: "https://t.me/+PEc2GpjasuExYzFl"
+  });
+}
